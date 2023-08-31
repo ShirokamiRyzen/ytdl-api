9c94671719ce257e9ad67dbc962a32d79f398257
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-31 15:36:58 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='yt-dlp-api',
+      version='0.1',
+      description='The ytdl API Server',
+      url='https://github.com/peter279k/yt-dlp-api',
+      author='ShirokamiRyzen',
+      author_email='infinixgaming360@gmail.com',
+      packages=['modules', 'routers'],
+      license='MIT',
+      python_requires=">=3.7",
+      zip_safe=False)
