8cfecb40d7319d341a5524f6d62f319db65349de
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-31 15:37:42 +0700

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='yt-dlp-api',
       version='0.1',
       description='The ytdl API Server',
-      url='https://github.com/peter279k/yt-dlp-api',
+      url='https://github.com/ShirokamiRyzen/ytdl-api',
       author='ShirokamiRyzen',
       author_email='infinixgaming360@gmail.com',
       packages=['modules', 'routers'],
